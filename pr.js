036b19a785f374fc530d155112212083d1853a64
Yanfii <patrickzhangyf@hotmail.com>
2017-06-02 11:07:35 -0400

    Update pr.js

--- pr.js
+++ pr.js
@@ -10,7 +10,7 @@ function buildPaymentRequest() {
   const supportedInstruments = [{
     supportedMethods: [
       'https://https://yanfii.github.io/test/',
-  //    'https://emerald-eon.appspot.com/bobpay',
+      'https://emerald-eon.appspot.com/bobpay',
       'basic-card'
     ],
   }];
