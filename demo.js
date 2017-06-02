2462924bd85c00f8f53486f641da6f2a03f99dc8
Yanfii <patrickzhangyf@hotmail.com>
2017-06-02 09:32:15 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,96 +1,91 @@
 /**
- * Builds PaymentRequest for credit cards, but does not show any UI yet.
- *
- * @return {PaymentRequest} The PaymentRequest oject.
+ * Initializes the payment request object.
+ * @return {PaymentRequest} The payment request object.
  */
+function buildPaymentRequest() {
+  if (!window.PaymentRequest) {
+    return null;
+  }
 
-const supportedInstruments = [{
-  supportedMethods: [
-'https://yanfii.github.io/test',
-    'https://emerald-eon.appspot.com/bobpay'
+  const supportedInstruments = [{
+    supportedMethods: [
+      'https://emerald-eon.appspot.com/alicepay',
+      'https://emerald-eon.appspot.com/bobpay',
     ],
-}];
+  }];
 
-function initPaymentRequest() {
-  let details = {
-    total: {label: 'Donation', amount: {currency: 'USD', value: '65.00'}},
-    displayItems: [
-      {
-        label: 'Original donation amount',
-        amount: {currency: 'USD', value: '55.00'},
+  const details = {
+    total: {
+      label: 'Donation',
+      amount: {
+        currency: 'USD',
+        value: '55.00',
       },
-      {
-        label: 'Friends and family discount',
-        amount: {currency: 'USD', value: '-10.00'},
+    },
+    displayItems: [{
+      label: 'Original donation amount',
+      amount: {
+        currency: 'USD',
+        value: '65.00',
       },
-    ],
+    }, {
+      label: 'Friends and family discount',
+      amount: {
+        currency: 'USD',
+        value: '-10.00',
+      },
+    }],
   };
-  return new PaymentRequest(supportedInstruments, details);
+
+  let request = null;
+
+  try {
+    request = new PaymentRequest(supportedInstruments, details);
+    if (request.canMakePayment) {
+      request.canMakePayment().then(function(result) {
+        info(result ? 'Can make payment' : 'Cannot make payment');
+      }).catch(function(err) {
+        error(err);
+      });
+    }
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+  }
+
+  return request;
 }
+
+let request = buildPaymentRequest();
 
 /**
- * Invokes PaymentRequest for credit cards.
- *
- * @param {PaymentRequest} request The PaymentRequest object.
+ * Launches payment request for Bob Pay.
  */
-function onBuyClicked(request) {
-  request.show().then(function(instrumentResponse) {
-    sendPaymentToServer(instrumentResponse);
-  })
-  .catch(function(err) {
-    ChromeSamples.setStatus(err);
-  });
-}
+function onBuyClicked() { // eslint-disable-line no-unused-vars
+  if (!window.PaymentRequest || !request) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
 
-/**
- * Simulates processing the payment data on the server.
- *
- * @param {PaymentResponse} instrumentResponse The payment information to
- * process.
- */
-function sendPaymentToServer(instrumentResponse) {
-  // There's no server-side component of these samples. No transactions are
-  // processed and no money exchanged hands. Instantaneous transactions are not
-  // realistic. Add a 2 second delay to make it seem more real.
-  window.setTimeout(function() {
-    instrumentResponse.complete('success')
-        .then(function() {
-          document.getElementById('result').innerHTML =
-              instrumentToJsonString(instrumentResponse);
-        })
-        .catch(function(err) {
-          ChromeSamples.setStatus(err);
-        });
-  }, 2000);
-}
-
-/**
- * Converts the payment instrument into a JSON string.
- *
- * @private
- * @param {PaymentResponse} instrument The instrument to convert.
- * @return {string} The JSON string representation of the instrument.
- */
-function instrumentToJsonString(instrument) {
-  let details = instrument.details;
-  details.cardNumber = 'XXXX-XXXX-XXXX-' + details.cardNumber.substr(12);
-  details.cardSecurityCode = '***';
-
-  return JSON.stringify({
-    methodName: instrument.methodName,
-    details: details,
-  }, undefined, 2);
-}
-
-const payButton = document.getElementById('buyButton');
-payButton.setAttribute('style', 'display: none;');
-if (window.PaymentRequest) {
-  let request = initPaymentRequest();
-  payButton.setAttribute('style', 'display: inline;');
-  payButton.addEventListener('click', function() {
-    onBuyClicked(request);
-    request = initPaymentRequest();
-  });
-} else {
-  ChromeSamples.setStatus('This browser does not support web payments');
+  try {
+    request.show()
+      .then(function(instrumentResponse) {
+        window.setTimeout(function() {
+          instrumentResponse.complete('success')
+            .then(function() {
+              done('This is a demo website. No payment will be processed.', instrumentResponse);
+            })
+            .catch(function(err) {
+              error(err);
+              request = buildPaymentRequest();
+            });
+        }, 500);
+      })
+      .catch(function(err) {
+        error(err);
+        request = buildPaymentRequest();
+      });
+  } catch (e) {
+    error('Developer mistake: \'' + e.message + '\'');
+    request = buildPaymentRequest();
+  }
 }
