485465dc4ab5f5fdd70c1a822dcba74b2259674c
Yanfii <patrickzhangyf@hotmail.com>
2017-06-05 17:08:58 -0400

    Update pr.js

--- pr/bob/pr.js
+++ pr/bob/pr.js
@@ -10,6 +10,7 @@ function buildPaymentRequest() {
   const supportedInstruments = [{
     supportedMethods: [
       'https://paymentweb-169813.appspot.com/test',
+      'basic-card',
     ],
   }];
 
